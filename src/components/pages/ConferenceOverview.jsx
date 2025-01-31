f520dac7345c3ae3e9697331a759fed610222340
Shashwat200419 <154683376+Shashwat200419@users.noreply.github.com>
2025-01-31 16:25:46 +0530

    dep

--- src/components/pages/ConferenceOverview.jsx
+++ src/components/pages/ConferenceOverview.jsx
@@ -8,7 +8,42 @@ export const ConferenceOverview = () => {
   return (
     <div className="mainDiv">
       <p className="ConferenceOverview" style={{backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '100%'}}><h1>Conference Overview</h1></p>
-      <h3 className="h3">Updating soon...</h3>
+      <div className="info">
+      <p>The conference covers diverse research areas, including but not limited to the following:</p>
+      <div className="information1">
+        <h3>Track 1 : Power Engineering</h3>
+        <ul>
+          <li>Power System</li>
+          <li>Power Electronics and Applications</li>
+          <li>Electric Transportation</li>
+          <li>Renewable Energy</li>
+          <li>Condition Monitoring of Power Apparatus</li>
+        </ul>
+      </div>
+      <div className="information2">
+        <h3> Track 2 : Control and Automation</h3>
+        <ul>
+          <li>Cyber Physical System</li>
+          <li>Robust and Adaptive Control</li>
+          <li>Robotics and Intelligent Control</li>
+          <li>Multiagent System</li>
+          <li>Modeling and Simulation</li>
+          <li>Internet of Things (IoT)</li>
+        </ul>
+      </div>
+      <div className="information3">
+        <h3> Track 3 : Artificial Intelligence (AI)</h3>
+        <ul>
+          <li>Machine Learning (ML) and Optimization</li>
+          <li>AI/ML in Electrical Engineering</li>
+          <li>Generative AI</li>
+          <li>Natural Language Processing (NLP)</li>
+          <li>AI/ML in Healthcare</li>
+          <li>Speech & Signal Processing</li>
+          <li>Computer Vision</li>
+        </ul>
+      </div>
+      </div>
       <FooterSection/>
     </div>
   )
